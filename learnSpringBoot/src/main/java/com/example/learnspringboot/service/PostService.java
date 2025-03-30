2a73f9d8681d45a6d3fe80b0a2b62eb7fe217555
최경영 <yeong7098@naver.com>
2025-03-31 02:32:49 +0900

    FEAT : redis 조회 후 없으면 디비 조회 postService

--- learnSpringBoot/src/main/java/com/example/learnspringboot/service/PostService.java
+++ learnSpringBoot/src/main/java/com/example/learnspringboot/service/PostService.java
@@ -13,24 +13,37 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+    private final RedisService redisService;
 
-    public List<Post> getAllPosts() {
+    public List<Post> getAllPost() {
         return postRepository.findAll();
     }
 
     public Post createPost(Post post) {
-        return postRepository.save(post);
+        Post savedPost = postRepository.save(post);
+        return  savedPost; // 캐시에 미리 저장을 해도 되지만, 조회시 저장을 해도 충분하다
     }
 
-    @Cacheable(value = "popularPosts", key = "#postId")
-    public String getPopularPost(Long postId) {
-        System.out.println("DB에서 게시글 조회");
-        String message = "인기 게시글 Id : " + postId;
-        return message;
+    public String getPost(Long postId){
+        String redisKey = "post_" + postId;
+
+        // Redis 먼저 조회
+        String cachedValue = redisService.getData(redisKey);
+        if(cachedValue != null) {
+            System.out.println("Redis에서 조회됨");
+            return cachedValue;
+        }
+        //없으면 디비 조회
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+        String value = "DB에서 조회된 게시글 : " + post.getId() + " / " + post.getId();
+
+        redisService.setDataWithTTL(redisKey,value,60);
+        return value;
     }
 
-    @CacheEvict(value = "popularPosts", key = "#postId")
-    public void invalidateCahce(Long postId) {
-        System.out.println("캐시 삭제됨");
+    public void invalidatePostCache(Long postId){
+        String redisKey = "post_" + postId;
+        redisService.deleteData(redisKey);
     }
 }
