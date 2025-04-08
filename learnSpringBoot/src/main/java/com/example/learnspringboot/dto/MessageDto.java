a129cbe63b02f2f0979f3fbb66ffa2d30f87b53d
최경영 <yeong7098@naver.com>
2025-04-08 15:59:17 +0900

    FEAT : MessageDto

---
+++ learnSpringBoot/src/main/java/com/example/learnspringboot/dto/MessageDto.java
@@ -0,0 +1,13 @@
+package com.example.learnspringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto {
+    private String id;
+    private String content;
+}
